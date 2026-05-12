ce562e17e31adfacacfbde5b4f50b3ab2d0e4fed
oumarmagassa <oumarmagassa75@gmail.com>
2026-05-12 17:33:35 +0200

    Update preparation_donnees.py

--- src/preparation_donnees.py
+++ src/preparation_donnees.py
@@ -1,1 +1,14 @@
 
+import pandas as pd
+
+# Chargement des données
+df = pd.read_csv("data/trafic_aerien.csv")
+
+# Affichage des premières lignes
+print(df.head())
+
+# Informations générales
+print(df.info())
+
+# Valeurs manquantes
+print(df.isnull().sum())
